27d618b9a754c1902e0fd816262a35008e58a45d
Baskanbetul <btlbskn@hotmail.com>
2022-08-09 09:39:08 -0600

    Figured out unique key issue

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,13 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postRequest = (newUrl) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(newUrl),
+  })
+  .then(response => response.json())
+  .catch(err => console.log(err))
+}
